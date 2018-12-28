39765b0559feebf07b65937a84dd90ddcfe4882f
adisbladis <adisbladis@gmail.com>
2018-12-28 20:21:48 +0000

    Add support for dates as version numbers

--- default.nix
+++ default.nix
@@ -1,55 +1,44 @@
 self: super:
 let
-  mkSrc = repoName: super.fetchFromGitHub (let
-    hashes = super.lib.importJSON (./. + "/repos/${repoName}.json");
-  in {
-    owner  = "ch11ng";
-    repo   = repoName;
-    rev    = hashes.rev;
-    sha256 = hashes.sha256;
-  });
-
   # TODO: Figure out how to avoid awkward nixpkgs import
   emacsWithPackages = import <nixpkgs/pkgs/build-support/emacs/wrapper.nix> (with super; {
     inherit (xorg) lndir;
     inherit lib makeWrapper stdenv runCommand;
   });
 
+  mkExDrv = emacsPackagesNg: name: args: let
+    repoMeta = super.lib.importJSON (./. + "/repos/${name}.json");
+  in emacsPackagesNg.melpaBuild (args // {
+      pname   = name;
+      ename   = name;
+      version = repoMeta.version;
+      recipe  = builtins.toFile "recipe" ''
+        (${name} :fetcher github
+          :repo "ch11ng/${name}")
+      '';
+
+      src = super.fetchFromGitHub {
+        owner  = "ch11ng";
+        repo   = name;
+        inherit (repoMeta) rev sha256;
+      };
+
+  });
+
 in {
-  # emacsPackagesNgFor = emacs: (super.emacsPackagesFor emacs // overridenAttrs);
   emacsPackagesNgFor = emacs: let
     emacsPackagesNg = super.emacsPackagesNgFor emacs;
 
     overridenAttrs = emacsPackagesNg // (with emacsPackagesNg; let
-      xelb = melpaBuild {
-        pname   = "xelb";
-        ename   = "xelb";
-        version = "9999";
-        recipe  = builtins.toFile "recipe" ''
-          (xelb :fetcher github
-                :repo "ch11ng/xelb")
-        '';
-
-        packageRequires = [ cl-generic super.emacs ];
-
-        src = mkSrc "xelb";
-
+      xelb = mkExDrv emacsPackagesNg "xelb" {
+        packageRequires = [ cl-generic emacs ];
         patches = [ ./0001-Fix-startup-using-non-posix-shells.patch ];
       };
-    in {
-      exwm = melpaBuild {
-        pname   = "exwm";
-        ename   = "exwm";
-        version = "9999";
-        recipe  = builtins.toFile "recipe" ''
-          (exwm :fetcher github
-                :repo "ch11ng/exwm")
-        '';
-
+      exwm = mkExDrv emacsPackagesNg "exwm" {
         packageRequires = [ xelb ];
-
-        src = mkSrc "exwm";
-     };
+      };
+    in {
+      inherit exwm xelb;
     });
   in overridenAttrs // {
     emacsWithPackages = emacsWithPackages overridenAttrs;
