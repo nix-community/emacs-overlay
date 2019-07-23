168536c0cb6b4238a4b64665d0a5367f215d47a8
adisbladis <adisbladis@gmail.com>
2019-07-23 21:34:36 +0100

    Add emacs package (updating from github emacs mirror)

--- default.nix
+++ default.nix
@@ -19,6 +19,24 @@ let
   });
 
 in {
+
+  emacsGit = let
+    repoMeta = super.lib.importJSON (./. + "/repos/emacs.json");
+    name = "emacs-git-${version}";
+    version = builtins.substring 0 7 repoMeta.rev;
+  in (super.emacs.override { srcRepo = true; }).overrideAttrs(old: {
+    inherit name version;
+    src = super.fetchFromGitHub {
+      owner = "emacs-mirror";
+      repo = "emacs";
+      inherit (repoMeta) sha256 rev;
+    };
+    patches = [
+      ./patches/tramp-detect-wrapped-gvfsd.patch
+      ./patches/clean-env.patch
+    ];
+  });
+
   emacsPackagesNgFor = emacs:
     (super.emacsPackagesNgFor emacs).overrideScope'(eself: esuper: {
       xelb = mkExDrv eself "xelb" {
@@ -28,4 +46,5 @@ in {
         packageRequires = [ eself.xelb ];
       };
     });
+
 }
--- test.nix
+++ test.nix
@@ -1,6 +1,9 @@
-with import <nixpkgs> {
-  overlays = [ (import ./default.nix) ];
-};
-emacsWithPackages(epkgs: [
+{ pkgs ? import <nixpkgs> { overlays = [ (import ./default.nix) ]; } }:
+
+let
+  package = pkgs.emacsGit;
+  emacsPackages = pkgs.emacsPackagesNgGen package;
+  emacsWithPackages = emacsPackages.emacsWithPackages;
+in emacsWithPackages(epkgs: [
   epkgs.exwm
 ])
