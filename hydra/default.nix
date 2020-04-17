1b31c94769bb2c377f63d9f8cb1cfef5180d4a44
adisbladis <adisbladis@gmail.com>
2020-04-17 02:09:18 +0100

    Add Hydra jobsets

---
+++ hydra/default.nix
@@ -0,0 +1,47 @@
+{ nixpkgs, declInput }:
+let
+  pkgs = import nixpkgs {};
+
+  mkJobset = {
+    nixpkgsRelease
+  }: {
+    enabled = 1;
+    hidden = false;
+    description = "Emacs-overlay jobset for nixpkgs branch ${nixpkgsRelease}";
+    nixexprinput = "src";
+    nixexprpath = "hydra/release.nix";
+    checkinterval = 300;
+    schedulingshares = 100;
+    enableemail = false;
+    emailoverride = "";
+    keepnr = 3;
+    inputs = {
+      src = {
+        type = "git";
+        value = "git://github.com/nix-community/emacs-overlay.git";
+        emailresponsible = false;
+      };
+      nixpkgs = {
+        type = "git";
+        value = "git://github.com/NixOS/nixpkgs-channels.git ${nixpkgsRelease}";
+        emailresponsible = false;
+      };
+    };
+  };
+
+  jobsets = {
+    unstable = mkJobset {
+      nixpkgsRelease = "nixos-unstable";
+    };
+  };
+
+in {
+  jobsets = pkgs.runCommand "spec.json" {} ''
+    cat <<EOF
+    ${builtins.toXML declInput}
+    EOF
+    cat > $out <<EOF
+    ${builtins.toJSON jobsets}
+    EOF
+  '';
+}
