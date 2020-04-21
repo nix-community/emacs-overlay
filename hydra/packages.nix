e2781ddd7ef35a89569a47a6f4493e1c2ca677fb
adisbladis <adisbladis@gmail.com>
2020-04-21 15:26:43 +0100

    hydra: Deduplicate hydra jobs
    By removing attrs both present in the "main" emacs set & in melpaPackages.

--- hydra/packages.nix
+++ hydra/packages.nix
@@ -14,7 +14,11 @@ let
 
   mkEmacsSet = emacs: let
     emacsPackages = lib.recurseIntoAttrs (pkgs.emacsPackagesFor emacs);
-  in builtins.removeAttrs emacsPackages dontBuildSubAttrs;
+    melpaPackages = emacsPackages.melpaPackages;
+    isMelpaPackage = n: v: lib.hasAttr n melpaPackages && v == melpaPackages.${n};
+    attrs = builtins.removeAttrs emacsPackages dontBuildSubAttrs;
+    # Remove melpaPackages from the main set to deduplicate hydra jobs
+  in lib.filterAttrs (n: v: ! isMelpaPackage n v) attrs;
 
 in {
   emacsPackages = mkEmacsSet pkgs.emacs;
