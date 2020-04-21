0300ae73bd1d506af7c9ff2a022f7ff36e0400e6
adisbladis <adisbladis@gmail.com>
2020-04-21 16:25:46 +0100

    Revert hydra filtering
    It doesn't work properly
    
    ---
    
    Revert "hydra: Attempt 2 at filtering melpa packages"
    
    This reverts commit db24a339273481b0699ffc04e89f8bfcc666be66.
    
    Revert "hydra: Deduplicate hydra jobs"
    
    This reverts commit e2781ddd7ef35a89569a47a6f4493e1c2ca677fb.

--- hydra/packages.nix
+++ hydra/packages.nix
@@ -12,18 +12,9 @@ let
     "melpaStablePackages"
   ];
 
-  isValid = d: let
-    r = builtins.tryEval (lib.isDerivation d && builtins.seq d.name (! (lib.attrByPath [ "meta" "broken" ] false d)) && "${d}" != "");
-  in r.success && r.value;
-
   mkEmacsSet = emacs: let
     emacsPackages = lib.recurseIntoAttrs (pkgs.emacsPackagesFor emacs);
-    melpaPackages = emacsPackages.melpaPackages;
-    # Dont iterate over melpa stable
-    attrs = builtins.removeAttrs emacsPackages dontBuildSubAttrs;
-    # Remove melpaPackages from the main set to deduplicate hydra jobs
-    isMelpaPackage = n: v: lib.hasAttr n melpaPackages && isValid v && isValid melpaPackages.${n} && v == melpaPackages.${n};
-  in lib.filterAttrs (n: v: ! isMelpaPackage n v) attrs;
+  in builtins.removeAttrs emacsPackages dontBuildSubAttrs;
 
 in {
   emacsPackages = mkEmacsSet pkgs.emacs;
