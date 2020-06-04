f0df033ca5751dfb8abd100a6c5d8f6a363b6a88
adisbladis <adisbladis@gmail.com>
2020-06-04 01:26:52 +0100

    Add emacsGcc to hydra jobset(s)

--- hydra/emacsen.nix
+++ hydra/emacsen.nix
@@ -5,8 +5,11 @@ let
       (import ../default.nix)
     ];
   };
+  inherit (pkgs) lib;
 
 in {
   inherit (pkgs) emacsUnstable emacsUnstable-nox;
   inherit (pkgs) emacsGit emacsGit-nox;
+} // lib.optionalAttrs (lib.hasAttr "libgccjit" pkgs) {
+  inherit (pkgs) emacsGcc;
 }
