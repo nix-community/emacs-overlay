a0260daa88fab08c9fe639df26a1570f14fa8825
talyz <kim.lindberger@gmail.com>
2020-07-18 14:43:29 +0200

    elisp.nix: fix spelling and indentation

--- elisp.nix
+++ elisp.nix
@@ -12,12 +12,16 @@ in {
   extraEmacsPackages ? epkgs: [],
   package ? pkgs.emacs,
   override ? (epkgs: epkgs)
-}: let
-  packages = parse.parsePackagesFromUsePackage config;
-  emacsPackages = pkgs.emacsPackagesGen package;
-  emacsWithPackages = emacsPackages.emacsWithPackages;
-in emacsWithPackages (epkgs: let
-  overriden = override epkgs;
-  usePkgs = builtins.map (name: overriden.${name}) packages;
-  extraPkgs = extraEmacsPackages overriden;
-in [ overriden.use-package ] ++ usePkgs ++ extraPkgs)
+}:
+  let
+    packages = parse.parsePackagesFromUsePackage config;
+    emacsPackages = pkgs.emacsPackagesGen package;
+    emacsWithPackages = emacsPackages.emacsWithPackages;
+  in
+    emacsWithPackages (epkgs:
+      let
+        overridden = override epkgs;
+        usePkgs = builtins.map (name: overridden.${name}) packages;
+        extraPkgs = extraEmacsPackages overridden;
+      in
+        [ overridden.use-package ] ++ usePkgs ++ extraPkgs)
