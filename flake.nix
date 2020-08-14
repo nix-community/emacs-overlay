06ff79cd70865849ac040d30be400bf340a5599f
John Axel Eriksson <john@insane.se>
2020-08-14 21:14:44 +0200

    Update flake.nix
    Remove edition field from flake.nix as it has been deprecated some time ago.

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
   description = "Bleeding edge Emacs overlay";
 
-  edition = 201909;
-
   outputs = { self }: {
     # self: super: must be named final: prev: for `nix flake check` to be happy
     overlay = final: prev:
