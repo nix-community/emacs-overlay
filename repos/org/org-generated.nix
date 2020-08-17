2f775d893614aa78d4e478f6b133be12b4b86a33
emacs-overlay <emacs-overlay@nix-community>
2020-08-17 00:37:51 +0000

    Updated repos/org

--- repos/org/org-generated.nix
+++ repos/org/org-generated.nix
@@ -4,10 +4,10 @@
       elpaBuild {
         pname = "org";
         ename = "org";
-        version = "20200810";
+        version = "20200817";
         src = fetchurl {
-          url = "https://orgmode.org/elpa/org-20200810.tar";
-          sha256 = "0q7m7d0wk1hwyqa6jhychgn0gqbj9i40cbrzxp760rfn0nihiy1i";
+          url = "https://orgmode.org/elpa/org-20200817.tar";
+          sha256 = "159hch9zls3apxq11c5rjpmci1avyl7q3cgsrqxwgnzy8c61104d";
         };
         packageRequires = [];
         meta = {
@@ -19,10 +19,10 @@
       elpaBuild {
         pname = "org-plus-contrib";
         ename = "org-plus-contrib";
-        version = "20200810";
+        version = "20200817";
         src = fetchurl {
-          url = "https://orgmode.org/elpa/org-plus-contrib-20200810.tar";
-          sha256 = "1fpg6gdr0sjj4ys7j7igjqfjpv4fybiq1hi70jgqbky5gawq1y0n";
+          url = "https://orgmode.org/elpa/org-plus-contrib-20200817.tar";
+          sha256 = "0n3fhcxjsk2w78p7djna4nlppa7ypjxzpq3r5dmzc8jpl71mipba";
         };
         packageRequires = [];
         meta = {
