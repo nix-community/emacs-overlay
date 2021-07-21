adisbladis/exwm-overlay

Add nongnu elpa

repos/nongnu/test.nix
@@ -1,0 +1,9 @@
+let
+
+  pkgs = import <nixpkgs> {
+    overlays = [
+      (import ../../default.nix)
+    ];
+  };
+
+in pkgs.emacsPackagesNg

nix-community/emacs-overlay

repos/nongnu/test.nix
@@ -1,0 +1,9 @@
+let
+
+  pkgs = import <nixpkgs> {
+    overlays = [
+      (import ../../default.nix)
+    ];
+  };
+
+in pkgs.emacsPackages