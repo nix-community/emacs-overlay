e6cf47e60505bab5e00a8a3d92ef5e714411088e
adisbladis <adisbladis@gmail.com>
2021-08-10 10:59:45 -0500

    hydra: Remove one level of indirection (emacsPackages) and deduplicate top-level packages

--- hydra/pkgs.nix
+++ hydra/pkgs.nix
@@ -3,8 +3,16 @@ nixpkgs:
 import nixpkgs {
   overlays = [
     (import ../default.nix)
-    (self: super: {
-      mkEmacsSet = emacs: { emacsPackages = super.recurseIntoAttrs (self.emacsPackagesFor emacs); };
+    (self: super: let
+      inherit (self) lib;
+    in {
+      # Build package sets and remove merged root-level packages since they are
+      # both present in each respective sub set and in the top-level
+      mkEmacsSet = emacs: super.recurseIntoAttrs (
+        lib.filterAttrs
+        (n: v: builtins.typeOf v == "set" && ! lib.isDerivation v)
+        (self.emacsPackagesFor emacs)
+      );
     })
   ];
 }
