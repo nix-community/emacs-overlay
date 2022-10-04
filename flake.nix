a6329647be7d68aa5525cdec42c3f5a0e4a11a39
Lin Jian <me@linj.tech>
2022-10-04 22:13:26 +0800

    Move default.nix into overlays dir
    This is to prepare for splitting the overlay into two seperated ones.

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     }: {
       # self: super: must be named final: prev: for `nix flake check` to be happy
       overlay = final: prev:
-        import ./default.nix final prev;
+        import ./overlays final prev;
     } // flake-utils.lib.eachDefaultSystem (system: (
       let
         pkgs = import nixpkgs {
