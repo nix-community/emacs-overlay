737fe5a55365f51bc3d47ffb0534a45cf0697c89
adisbladis <adisbladis@gmail.com>
2023-06-02 00:33:07 +1200

    Remove exwm repo
    Exwm is in elpa and the update cadence doesn't motivate that we have the special handling around any more.
    
    This removal feels quite funny given that this repository started it's
    life as an overlay precisely to get bleeding edge exwm versions, and then grew to other packages from there.
    
    So long exwm repo!

--- overlays/package.nix
+++ overlays/package.nix
@@ -1,30 +1,4 @@
 self: super:
-let
-  mkExDrv = emacsPackages: name: args:
-    let
-      repoMeta = super.lib.importJSON (../repos/exwm/. + "/${name}.json");
-    in
-    emacsPackages.melpaBuild (
-      args // {
-        pname = name;
-        ename = name;
-        version = repoMeta.version;
-        commit = repoMeta.rev;
-
-        recipe = builtins.toFile "recipe" ''
-          (${name} :fetcher github
-          :repo "ch11ng/${name}")
-        '';
-
-        src = super.fetchFromGitHub {
-          owner = "ch11ng";
-          repo = name;
-          inherit (repoMeta) rev sha256;
-        };
-      }
-    );
-
-in
 {
   emacsPackagesFor = emacs: (
     (super.emacsPackagesFor emacs).overrideScope' (
@@ -46,21 +20,12 @@ in
             generated = ../repos/nongnu/nongnu-generated.nix;
           };
 
-          epkgs = esuper.override {
+        in
+          esuper.override {
             inherit melpaStablePackages melpaPackages elpaPackages
               nongnuPackages;
-          };
+          }
 
-        in
-        epkgs // {
-          xelb = mkExDrv eself "xelb" {
-            packageRequires = [ eself.cl-generic eself.emacs ];
-          };
-
-          exwm = mkExDrv eself "exwm" {
-            packageRequires = [ eself.xelb ];
-          };
-        }
     )
   );
 
