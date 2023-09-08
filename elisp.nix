96f28387962e0cbe7a1c861b47411246bd4d5e1b
Lin Jian <me@linj.tech>
2023-09-08 08:42:21 +0800

    elisp.nix: add version for default.el package
    A recent change[1] in Nixpkgs makes version attribute non-optional.
    
    Fixes: https://github.com/nix-community/emacs-overlay/issues/356
    
    [1]: https://github.com/NixOS/nixpkgs/pull/253448

--- elisp.nix
+++ elisp.nix
@@ -84,6 +84,7 @@ emacsWithPackages (epkgs:
                 if defaultInitFile.name == defaultInitFileName
                 then defaultInitFile
                 else throw "name of defaultInitFile must be ${defaultInitFileName}";
+            version = "0.1.0";
             packageRequires = usePkgs;
           };
   in
