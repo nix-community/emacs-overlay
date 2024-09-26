c691136bcb2dd7486e6dae9eeb3f30c9c3a4bf98
Brian Leung <leungbk@posteo.net>
2024-09-26 08:31:21 -0700

    Use bytecomp-revert.patch for unstable Emacsen

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -122,12 +122,32 @@ let
   emacs-unstable = let base = super.lib.makeOverridable (mkGitEmacs "emacs-unstable" ../repos/emacs/emacs-unstable.json) { withSQLite3 = true; withWebP = true; };
                        # TODO: remove when we drop support for < 23.05, and instead move withTreeSitter to the above line with the other arguments
                        maybeOverridden = if (super.lib.hasAttr "treeSitter" base || super.lib.hasAttr "withTreeSitter" base) then base.override { withTreeSitter = true; } else base;
-                   in maybeOverridden;
+                       emacs = emacs-unstable;
+                   in maybeOverridden.overrideAttrs (
+                     oa: {
+                       patches = oa.patches ++ [
+                         # XXX: #318
+                         ./bytecomp-revert.patch
+                       ];
+                       passthru = oa.passthru // {
+                         pkgs = oa.passthru.pkgs.overrideScope (eself: esuper: { inherit emacs; });
+                       };
+                     });
 
   emacs-unstable-pgtk = let base = super.lib.makeOverridable (mkGitEmacs "emacs-unstable" ../repos/emacs/emacs-unstable.json) { withSQLite3 = true; withWebP = true; withPgtk = true; };
                             # TODO: remove when we drop support for < 23.05, and instead move withTreeSitter to the above line with the other arguments
                             maybeOverridden = if (super.lib.hasAttr "treeSitter" base || super.lib.hasAttr "withTreeSitter" base) then base.override { withTreeSitter = true; } else base;
-                        in maybeOverridden;
+                            emacs = emacs-unstable-pgtk;
+                        in maybeOverridden.overrideAttrs (
+                          oa: {
+                            patches = oa.patches ++ [
+                              # XXX: #318
+                              ./bytecomp-revert.patch
+                            ];
+                            passthru = oa.passthru // {
+                              pkgs = oa.passthru.pkgs.overrideScope (eself: esuper: { inherit emacs; });
+                            };
+                          });
 
   emacs-lsp = let base = super.lib.makeOverridable (mkGitEmacs "emacs-lsp" ../repos/emacs/emacs-lsp.json) { };
                   # TODO: remove when we drop support for < 23.05, and instead move withTreeSitter to the above line with the other arguments
