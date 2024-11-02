711566e17e7b414a24973dfd00a63eb62efb3836
adisbladis <adisbladis@gmail.com>
2024-11-02 13:11:33 +0000

    flake: Remove flake-utils

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
   };
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
@@ -16,13 +15,37 @@
     { self
     , nixpkgs
     , nixpkgs-stable
-    , flake-utils
     }:
     let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+
       importPkgs = path: attrs: import path (attrs // {
         config.allowAliases = false;
         overlays = [ self.overlays.default ];
       });
+
+      packages' = forAllSystems (system: (
+        let
+          pkgs = importPkgs nixpkgs { inherit system; };
+          inherit (pkgs) lib;
+
+          overlayAttributes = lib.pipe (import ./. pkgs pkgs) [
+            builtins.attrNames
+            (lib.partition (n: lib.isDerivation pkgs.${n}))
+          ];
+          attributesToAttrset = attributes: lib.pipe attributes [
+            (map (n: lib.nameValuePair n pkgs.${n}))
+            lib.listToAttrs
+          ];
+
+        in
+        {
+          lib = attributesToAttrset overlayAttributes.wrong;
+          packages = attributesToAttrset overlayAttributes.right;
+        }
+      ));
+
     in
     {
       # self: super: must be named final: prev: for `nix flake check` to be happy
@@ -34,12 +57,8 @@
       # for backward compatibility, is safe to delete, not referenced anywhere
       overlay = self.overlays.default;
 
-      # Run Hercules CI for these systems.
-      herculesCI.ciSystems = [ "x86_64-linux" ];
-
-    } // flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ] (system:
-    {
       hydraJobs =
+        lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system:
         let
           mkHydraJobs = pkgs:
             let
@@ -66,26 +85,9 @@
         {
           "stable" = mkHydraJobs (importPkgs nixpkgs-stable { inherit system; });
           "unstable" = mkHydraJobs (importPkgs nixpkgs { inherit system; });
-        };
-    }) // flake-utils.lib.eachDefaultSystem (system: (
-      let
-        pkgs = importPkgs nixpkgs { inherit system; };
-        inherit (pkgs) lib;
+        });
 
-        overlayAttributes = lib.pipe (import ./. pkgs pkgs) [
-          builtins.attrNames
-          (lib.partition (n: lib.isDerivation pkgs.${n}))
-        ];
-        attributesToAttrset = attributes: lib.pipe attributes [
-          (map (n: lib.nameValuePair n pkgs.${n}))
-          lib.listToAttrs
-        ];
-
-      in
-      {
-        lib = attributesToAttrset overlayAttributes.wrong;
-        packages = attributesToAttrset overlayAttributes.right;
-      }
-    ));
-
+      packages = forAllSystems (system: packages'.${system}.packages);
+      lib = forAllSystems (system: packages'.${system}.lib);
+    };
 }
