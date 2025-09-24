6179c47ccfba5c0211c66a9e2caaf05ca99cb387
Stefan Frijters <sfrijters@gmail.com>
2025-09-24 11:02:32 +0200

    flake: bump nixpkgs-stable from 24.11 to 25.05

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs =
