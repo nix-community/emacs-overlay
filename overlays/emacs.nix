477d0aaea502bcbe8bba7471e6eea18a34f1636a
Brian Leung <leungbk@posteo.net>
2026-02-18 12:54:58 -0800

    Remove emacsLsp and use upstream nixpkgs' patch set
    emacsLsp is based on Emacs 28, which is no longer in nixpkgs.
    
    Because we remove the native-comp workaround for Emacs 28, we now rely
    on the upstream patch provided in nixpkgs.

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -25,8 +25,6 @@ let
               inherit (repoMeta) version;
               src = fetcher (builtins.removeAttrs repoMeta [ "type" "version" ]);
 
-              patches = [ ];
-
               # fixes segfaults that only occur on aarch64-linux (#264)
               configureFlags = old.configureFlags ++
                                super.lib.optionals (super.stdenv.isLinux && super.stdenv.isAarch64)
@@ -36,43 +34,7 @@ let
                 substituteInPlace lisp/loadup.el \
                 --replace-warn '(emacs-repository-get-version)' '"${repoMeta.rev}"' \
                 --replace-warn '(emacs-repository-get-branch)' '"master"'
-              '' +
-              # XXX: Maybe remove when emacsLsp updates to use Emacs
-              # 29.  We already have logic in upstream Nixpkgs to use
-              # a different patch for earlier major versions of Emacs,
-              # but the major version for emacsLsp follows the format
-              # of version YYYYMMDD, as opposed to version (say) 29.
-              # Removing this here would also require that we don't
-              # overwrite the patches attribute in the overlay to an
-              # empty list since we would then expect the Nixpkgs
-              # patch to be used. Not sure if it's better to rely on
-              # upstream Nixpkgs since it's cumbersome to wait for
-              # things to get merged into master.
-                (super.lib.optionalString ((old ? NATIVE_FULL_AOT) || (old ? env.NATIVE_FULL_AOT))
-                    (let backendPath = (super.lib.concatStringsSep " "
-                      (builtins.map (x: ''\"-B${x}\"'') ([
-                        # Paths necessary so the JIT compiler finds its libraries:
-                        "${super.lib.getLib self.libgccjit}/lib"
-                        "${super.lib.getLib self.libgccjit}/lib/gcc"
-                        "${super.lib.getLib self.stdenv.cc.libc}/lib"
-		      ] ++ super.lib.optionals (self.stdenv.cc?cc.libgcc) [
-			"${super.lib.getLib self.stdenv.cc.cc.libgcc}/lib"
-		      ] ++ [
-
-                        # Executable paths necessary for compilation (ld, as):
-                        "${super.lib.getBin self.stdenv.cc.cc}/bin"
-                        "${super.lib.getBin self.stdenv.cc.bintools}/bin"
-                        "${super.lib.getBin self.stdenv.cc.bintools.bintools}/bin"
-                      ] ++ super.lib.optionals (self.stdenv.hostPlatform.isDarwin && self ? apple-sdk) [
-                        # The linker needs to know where to find libSystem on Darwin.
-                        "${self.apple-sdk.sdkroot}/usr/lib"
-                      ])));
-                     in ''
-                        substituteInPlace lisp/emacs-lisp/comp.el --replace-warn \
-                            "(defcustom comp-libgccjit-reproducer nil" \
-                            "(setq native-comp-driver-options '(${backendPath}))
-(defcustom comp-libgccjit-reproducer nil"
-                    ''));
+              '';
             }
           )
         )
@@ -223,5 +185,5 @@ in
   emacsUnstablePgtk = builtins.trace "emacsUnstablePgtk has been renamed to emacs-unstable-pgtk, please update your expression." emacs-unstable-pgtk;
   emacsGitNox = builtins.trace "emacsGitNox has been renamed to emacs-git-nox, please update your expression." emacs-git-nox;
   emacsUnstableNox = builtins.trace "emacsUnstableNox has been renamed to emacs-unstable-nox, please update your expression." emacs-unstable-nox;
-  emacsLsp = builtins.trace "emacsLsp has been renamed to emacs-lsp, please update your expression." emacs-lsp;
+  emacsLsp = throw "emacsLsp has been removed, please update your expression.";
 }
